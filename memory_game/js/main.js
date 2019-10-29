120983327ce2476d9807704a4083cf6a656990e2
Sinara <sinaramartins@gmail.com>
2019-10-29 02:35:51 -0400

    Finished game's logic using DOM
    When the user clicks on a card, the image
    with the face of that card is displayed.
    If the user selectes two cards, an alert
    pops up letting the user know if the two
    cards match. Also added a reset game button
    that reloads the page to the HTML code.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,6 +27,7 @@ cardImage: 'images/king-of-diamonds.png',
 const cardsInPlay = [];
 
 function checkForMatch (){
+	 
 	if (cardsInPlay.length >= 2){
 		if ( cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -37,14 +38,37 @@ function checkForMatch (){
 };
 
 
-function flipCard (cardId){
+function flipCard (){
+	var cardId = this.getAttribute('data-id');
+	// getAttribute method to get the data-id attribute of 
+	// the card that was just clicked and store it in a 
+	// variable cardId.
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	// use the setAttribute method to update the src attribute 
+	// to the image of the card that was just clicked (the image
+	// source is in the cardImage property in that card's object 
+	// in the cards array).
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard (){
+	for (var i = 0; i < cards.length; i++ ) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		// data- attributes are meant to store data about an 
+		// element that is not tied to a style. 
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+createBoard();
+
+
 
